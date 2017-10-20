22e6d0995f52b10a0c3af7d5d0857de9dea32bf5
DungNA8 <me@anhdung.net>
2017-10-20 21:36:53 +0700

    Code something

---
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/config/Constants.java
@@ -0,0 +1,9 @@
+package ise1005.edu.fpt.vn.myrestaurant.config;
+
+/**
+ * Created by DungNA on 10/20/17.
+ */
+
+public class Constants {
+    public static final String API_URL = "https://prm391.anhdung.net/";
+}
