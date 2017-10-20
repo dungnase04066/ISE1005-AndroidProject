d3080bdf2040958ee22665fad85ce086ee58f7d1
DungNA8 <me@anhdung.net>
2017-10-20 22:04:35 +0700

    Add some DTO

---
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/dto/ProductDTO.java
@@ -0,0 +1,73 @@
+package ise1005.edu.fpt.vn.myrestaurant.dto;
+
+/**
+ * Created by DungNA on 10/20/17.
+ */
+
+public class ProductDTO {
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(int id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductDTO that = (ProductDTO) o;
+        return id == that.id;
+
+    }
+
+    @Override
+    public String toString() {
+        return "ProductDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
