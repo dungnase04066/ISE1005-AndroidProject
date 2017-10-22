7ca23c886232b6b296ebec2b6f41e51588b6cb73
Frank <frank.xayachack@gmail.com>
2017-10-22 18:50:47 +0700

    Coding some thing=)))

---
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/manager/User.java
@@ -0,0 +1,15 @@
+package ise1005.edu.fpt.vn.myrestaurant.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ise1005.edu.fpt.vn.myrestaurant.R;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
