a5f252aa0d25aad02a1f97ff92513162ca80ece8
authinh <auvanthinh@hotmail.com>
2017-10-22 23:53:26 +0800

    Merge branch 'master' of https://github.com/dungnase04066/ISE1005-AndroidProject create screen staff
    # Conflicts:
    #	MyRestaurant/app/build.gradle
    #	MyRestaurant/app/src/main/AndroidManifest.xml
    #	MyRestaurant/app/src/main/res/values/dimens.xml
    #	MyRestaurant/app/src/main/res/values/strings.xml

---
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/staff/OrderForm.java
@@ -0,0 +1,15 @@
+package ise1005.edu.fpt.vn.myrestaurant.staff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ise1005.edu.fpt.vn.myrestaurant.R;
+
+public class OrderForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_form);
+    }
+}
