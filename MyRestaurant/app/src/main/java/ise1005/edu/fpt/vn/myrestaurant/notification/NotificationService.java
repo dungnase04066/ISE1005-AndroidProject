f0cf7c50e93a51132f882a1a781956e274faeec0
DungNA8 <me@anhdung.net>
2017-10-23 21:57:59 +0700

    add notification

--- MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/notification/NotificationService.java
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/notification/NotificationService.java
@@ -3,34 +3,19 @@ package ise1005.edu.fpt.vn.myrestaurant.notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.NotificationCompat;
-
-import com.pusher.client.Pusher;
-import com.pusher.client.PusherOptions;
-import com.pusher.client.channel.Channel;
-import com.pusher.client.channel.SubscriptionEventListener;
+import android.util.Log;
 
 import ise1005.edu.fpt.vn.myrestaurant.R;
-import ise1005.edu.fpt.vn.myrestaurant.config.Constants;
 import ise1005.edu.fpt.vn.myrestaurant.util.LoginActivity;
 
-/**
- * Created by DungNA on 10/22/17.
- */
-
-public class Notification extends Service {
-    private Pusher pusher;
-    private Channel channel;
-
-    public Notification() {
-        PusherOptions options = new PusherOptions();
-        options.setCluster("ap1");
-        pusher = new Pusher(Constants.PUSHER_KEY, options);
-        channel = pusher.subscribe(Constants.PUSHER_CHANNEL);
+public class NotificationService extends Service {
+    public NotificationService() {
     }
 
     @Nullable
@@ -39,15 +24,17 @@ public class Notification extends Service {
         return null;
     }
 
-    public void setEvent(String eventName) {
-//        channel.bind(eventName, new SubscriptionEventListener() {
-//            @Override
-//            public void onEvent(String channelName, String eventName, final String data) {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Log.d("NotificationService", "Service Started");
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent i = Intent.makeRestartActivityTask(new ComponentName(this, LoginActivity.class));
         PendingIntent resultPendingIntent =
                 PendingIntent.getActivity(
                         this,
                         0,
-                        new Intent(this, LoginActivity.class),
+                        i,
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
@@ -55,13 +42,14 @@ public class Notification extends Service {
         mBuilder.setContentTitle("Notification Alert, Click Me!");
         mBuilder.setContentText("Hi, This is Android Notification Detail!");
         mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(100, mBuilder.build());
-//            }
-//        });
+        stopSelf();
+        return START_STICKY;
     }
 
-    public void connect() {
-        pusher.connect();
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("NotificationService", "Service Destroyed");
     }
 }
