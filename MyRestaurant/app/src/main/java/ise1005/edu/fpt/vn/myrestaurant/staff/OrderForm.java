257b50f52eff0c640934a3d8d0d47167f50d9f11
authinh <auvanthinh@hotmail.com>
2017-10-24 02:16:56 +0800

    Merge branch 'master' of https://github.com/dungnase04066/ISE1005-AndroidProject - add class createorderdetailtask to add orderdetail when staff touch sumbit button (in process) - add class createorderstask to add order when staff touch sumbit button (in process) - update class OrderForm.java to get list product (in process) - update screen activity_order_form.xml (in process)
    # Conflicts:
    #	MyRestaurant/app/build.gradle
    #	MyRestaurant/app/src/main/AndroidManifest.xml
    #	MyRestaurant/app/src/main/res/values/dimens.xml
    #	MyRestaurant/app/src/main/res/values/strings.xml

--- MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/staff/OrderForm.java
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/staff/OrderForm.java
@@ -2,14 +2,23 @@ package ise1005.edu.fpt.vn.myrestaurant.staff;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import ise1005.edu.fpt.vn.myrestaurant.R;
+import ise1005.edu.fpt.vn.myrestaurant.asynctask.IAsyncTaskHandler;
 
-public class OrderForm extends AppCompatActivity {
+public class OrderForm extends AppCompatActivity implements IAsyncTaskHandler {
+
+    private ListView listProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_form);
+    }
+
+    @Override
+    public void onPostExecute(Object o) {
+
     }
 }
