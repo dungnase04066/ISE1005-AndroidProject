059db2ba8407c234c4fd696fa01a9c82a0a6f2f7
DungNA8 <me@anhdung.net>
2017-10-25 12:18:48 +0700

    add pusher and notification for android app

--- MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/notification/Notification.java
+++ MyRestaurant/app/src/main/java/ise1005/edu/fpt/vn/myrestaurant/notification/Notification.java
@@ -5,9 +5,12 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 
 import com.pusher.client.Pusher;
 import com.pusher.client.PusherOptions;
@@ -22,15 +25,27 @@ import ise1005.edu.fpt.vn.myrestaurant.util.LoginActivity;
  * Created by DungNA on 10/22/17.
  */
 
-public class Notification extends Service {
+public class Notification extends Service implements SubscriptionEventListener {
     private Pusher pusher;
     private Channel channel;
+    private String eventName;
 
     public Notification() {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Bundle extras = intent.getExtras();
         PusherOptions options = new PusherOptions();
         options.setCluster("ap1");
         pusher = new Pusher(Constants.PUSHER_KEY, options);
         channel = pusher.subscribe(Constants.PUSHER_CHANNEL);
+        channel.bind(extras.getString("eventName"), this);
+        Log.d("NotificationService","Event name: "+extras.getString("eventName"));
+        connect();
+//        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Nullable
@@ -39,29 +54,19 @@ public class Notification extends Service {
         return null;
     }
 
-    public void setEvent(String eventName) {
-//        channel.bind(eventName, new SubscriptionEventListener() {
-//            @Override
-//            public void onEvent(String channelName, String eventName, final String data) {
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        new Intent(this, LoginActivity.class),
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
+    public void connect() {
+        pusher.connect();
+        Log.d("NotificationService", "Connected to pusher");
+    }
+
+    @Override
+    public void onEvent(String channelName, String eventName, final String data) {
+        Log.d("NotificationService", "Received event");
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
         mBuilder.setSmallIcon(R.mipmap.ic_shop);
         mBuilder.setContentTitle("Notification Alert, Click Me!");
         mBuilder.setContentText("Hi, This is Android Notification Detail!");
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         mNotificationManager.notify(100, mBuilder.build());
-//            }
-//        });
-    }
-
-    public void connect() {
-        pusher.connect();
     }
 }
