13ac23932d23786a4d1b1f8ff98e5ddd1c1acc72
DungNA <me@anhdung.net>
2017-11-01 21:58:38 +0700

    update service

--- MyRestaurantService/t7/orderdetail/get.php
+++ MyRestaurantService/t7/orderdetail/get.php
@@ -3,7 +3,7 @@ $table_id = intval($_GET['table_id']);
 $id = intval($_GET['id']);
 if($id>0 || $table_id>0){
     if($table_id>0){
-        $sql =  "SELECT `id` FROM `orders` WHERE `table_id` = {$table_id} ORDER BY id DESC";
+        $sql =  "SELECT `id` FROM `orders` WHERE `table_id` = {$table_id} AND `status`=0 ORDER BY id DESC";
         $rs = $mysqli->query($sql);
         $od = $rs->fetch_all(MYSQLI_ASSOC);
         if(count($od)>0) $id = $od[0]['id'];
